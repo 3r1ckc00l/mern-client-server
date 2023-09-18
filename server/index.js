0e754d95d76c8ea40ae0e093f6defd69481800e8
3ryh_c00l <3rikpas@gmail.com>
2023-09-18 13:29:20 +0700

    connect with mongoose

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
 
 const app = express();
+
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
@@ -14,7 +18,7 @@ const CONNECTION_URL = 'mongodb+srv://admin:3R1ckpas@cluster0.m43kshd.mongodb.ne
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(()=>app.listen(PORT, ()=>console.log('Server running on port: ${PORT}')))
-    .catch((error)=>console.log(error.message));
+    .then(() => app.listen(PORT, () => console.log('Server running on port: ${PORT}')))
+    .catch((error) => console.log(error.message));
 
-mongoose.set('useFindAndModify', false);
+//mongoose.set('strictQuery', false);
